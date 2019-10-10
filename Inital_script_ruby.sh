f615250695cff81781e29e2e5468ac69f6a74adf
LinkPhoenix <linkblue11@outlook.com>
2019-10-10 12:22:32 +0200

    create gemfile

--- Inital_script_ruby.sh
+++ Inital_script_ruby.sh
@@ -2,6 +2,15 @@ echo "create Gemfile"
 
 touch Gemfile
 
+echo "Edit the Gemile"
+
+echo "ruby '2.5.1'" >> Gemfile
+echo "em 'rspec'"  >> Gemfile
+echo "gem 'pry'" >> Gemfile
+echo "gem 'rubocop', '~> 0.57.2'" >> Gemfile
+echo "gem 'rubygems'" >> Gemfile
+echo "gem 'nokogiri'" >> Gemfile
+
 echo "Initial Bundle"
 
 bundle install
