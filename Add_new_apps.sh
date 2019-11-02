7d8aa4a489a077eb67208812512b8f3ffa3f2850
LinkPhoenix <linkblue11@outlook.com>
2019-11-03 00:27:15 +0100

    add some new tricks

--- Add_new_apps.sh
+++ Add_new_apps.sh
@@ -1,15 +1,48 @@
+echo "Install Zsh"
+
+sudo apt install zsh
+chsh -s $(which zsh) #set zsh as default shell
+
+echo "Backup zsh conf"
+
+cd
+cp ~/.zshrc ~/.zshrc.orig
+
+echo "Install Oh My ZSH"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" #Via CURL
+#sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" #Via WGET
+
 echo "Install Terminator Terminal"
 
 sudo apt install terminator
- 
-echo "install Snap On Elementary OS"
- 
-sudo apt update
-sudo apt install snapd
 
-echo "Install Visual Code with Snap"
- 
-sudo snap install --classic code
+echo "Install Android File Transfer"
+
+# https://askubuntu.com/questions/1038145/trying-to-install-android-file-transfer-ubuntu-18-04-bionic
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0BB4A1B2FA1A38EB
+sudo add-apt-repository "deb http://ppa.launchpad.net/samoilov-lex/aftl-stable/ubuntu artful main"
+sudo apt update
+sudo apt install android-file-transfer
+
+echo "install terminator"
+
+sudo apt install terminator
+
+# echo "install snap"
+
+# sudo apt install snapd
+
+echo "install Visual Code"
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install code # or code-insiders
 
 echo "Install Slack Desktop"
 
@@ -19,12 +52,10 @@ sudo apt install ./slack-desktop-*.deb
 
 echo "Install Spotify with Snap"
 
-snap install spotify
-
-#echo "Install Spotify"
-#curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
-#echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-#sudo apt-get update && sudo apt-get install spotify-client
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt update
+sudo apt install spotify-client
 
 echo "Install vlc Media Player"
 
@@ -53,3 +84,7 @@ sudo apt install qbittorrent
 echo "Install NeoFetch"
 
 sudo apt install neofetch
+
+echo "Install tree"
+
+sudo apt install tree
--- Setup_Elementary_OS.sh
+++ Setup_Elementary_OS.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env sh
 echo "change the default terminal by Terminator"
 
 gsettings set org.gnome.desktop.default-applications.terminal exec terminator
@@ -6,4 +7,49 @@ gsettings set org.gnome.desktop.default-applications.terminal exec-arg ''
 # echo "Default value for default terminal"
 # gsettings reset org.gnome.desktop.default-applications.terminal exec
 # gsettings reset org.gnome.desktop.default-applications.terminal exec-arg
-#
+
+echo "install Mac OS Theme"
+
+#https://github.com/surajmandalcell/elementary-x
+git clone https://github.com/surajmandalcell/elementary-x.git ~/.themes/elementary-x
+gsettings set org.gnome.desktop.interface gtk-theme "elementary-x"
+
+echo "Config DARK MODE"
+
+#https://askubuntu.com/questions/769417/how-to-change-global-dark-theme-on-and-off-through-terminal
+#https://askubuntu.com/questions/20414/find-and-replace-text-within-a-file-using-commands
+sed -i 's/gtk-application-prefer-dark-theme=0/gtk-application-prefer-dark-theme=1/g' ~/.config/gtk-3.0/settings.ini
+
+echo "Install icon theme Mac os La Sierra"
+
+https://github.com/btd1337/La-Sierra-Icon-Theme
+git clone https://github.com/btd1337/La-Sierra-Icon-Theme ~/.icons/La-Sierra
+gsettings set org.gnome.desktop.interface icon-theme "La-Sierra"
+
+echo "Add trash icone in the dock"
+
+#https://elementaryos.stackexchange.com/questions/18404/where-is-the-trash-icon/21088#21088
+touch $HOME/.config/plank/dock1/launchers/trash.dockitem
+sed -i 's%Launcher=%Launcher=docklet://trash%g' $HOME/.config/plank/dock1/launchers/trash.dockitem
+killall plank
+
+
+echo "Install Gparted"
+
+sudo apt install gparted
+
+# echo "Disable touchscreen at startup"
+
+# sudo -i
+# mkdir /autostart_script
+# touch /autostart_script/disable_touchscreen.sh
+# echo '#!/usr/bin/env bash' >>/autostart_script/disable_touchscreen.sh
+# echo 'xinput disable 12' >>/autostart_script/disable_touchscreen.sh
+# chmod +x /autostart_script/disable_touchscreen.sh
+
+# touch /etc/init/disable_touchscreen.conf
+# echo 'description     "Disable the Touchscreen"' >>/etc/init/disable_touchscreen.conf
+# echo 'start on startup' >>/etc/init/disable_touchscreen.conf
+# echo 'task' >>/etc/init/disable_touchscreen.conf
+# echo 'exec /autostart_script/disable_touchscreen.sh' >>/etc/init/disable_touchscreen.conf
+# exit
